eec4b5a5e129c86d0ff72fc0169cdfc0482bff6b
JuJiiz <ju.jiiz.1579@gmail.com>
2017-12-26 15:26:00 +0700

    15:25 26/12/2560 nav OPT & nav TR14

--- app/src/main/java/com/example/jujiiz/mis/controllers/OPTActivity.java
+++ app/src/main/java/com/example/jujiiz/mis/controllers/OPTActivity.java
@@ -2,13 +2,25 @@ package com.example.jujiiz.mis.controllers;
 
 import android.app.LocalActivityManager;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TabHost;
 
 import com.example.jujiiz.mis.R;
+import com.example.jujiiz.mis.models.ModelNavClick;
 
-public class OPTActivity extends AppCompatActivity {
+public class OPTActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     LocalActivityManager mLocalActivityManager;
 
@@ -16,6 +28,16 @@ public class OPTActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opt);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         mLocalActivityManager = new LocalActivityManager(this, false);
         mLocalActivityManager.dispatchCreate(savedInstanceState);
@@ -27,24 +49,53 @@ public class OPTActivity extends AppCompatActivity {
                 .setIndicator("ข้อมูล อปท.")
                 .setContent(new Intent(this, OPTFormActivity.class));
 
-        /*TabHost.TabSpec tabSpec2 = tabHost.newTabSpec("tab2")
-                .setIndicator("Jelly Bean")
-                .setContent(new Intent(this, Tab2.class));*/
+        TabHost.TabSpec tabSpec2 = tabHost.newTabSpec("tab2")
+                .setIndicator("หมู่บ้านในเขตการปกครอง")
+                .setContent(new Intent(this, VillageActivity.class));
 
         tabHost.addTab(tabSpec);
-        //tabHost.addTab(tabSpec2);
+        tabHost.addTab(tabSpec2);
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        mLocalActivityManager.dispatchPause(!isFinishing());
-
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.opt, menu);
+        return true;
+    }*/
+
+    /*@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }*/
+
+    @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    protected void onResume() {
-        super.onResume();
-        mLocalActivityManager.dispatchResume();
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        ModelNavClick.displaySelectedScreen(this, id);
+        return true;
     }
 }
